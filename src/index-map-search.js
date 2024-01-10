efc25228b58fae0b8dde49318a2e0a1130d46e5a
riancintiyo <cintiyo.ryan@gmail.com>
2024-01-10 23:45:34 +0700

    feat(index-map-search.js): Adding search location part 2

--- src/index-map-search.js
+++ src/index-map-search.js
@@ -8,75 +8,64 @@ dotenv.config();
 const genAI = new GoogleGenerativeAI(process.env.API_KEY || 'default_api_key');
 
 const getPlace = async (place) => {
-	const browser = await pw.chromium.launch({ headless: false });
+	let selectedUrl = '';
+	const browser = await pw.chromium.launch({ headless: true });
     const page = await browser.newPage();
 
 	await page.goto('https://www.google.com/maps');
 	await page.waitForSelector('.searchboxinput.xiQnY');
-
-	const inputEl = page.locator('.searchboxinput.xiQnY');
-	const searchBtn = page.locator('#searchbox-searchbutton');
-
-	await inputEl.fill(`${place}`);
-	await searchBtn.click();
-
-	// Wait for either of the elements to appear
+	await page.locator('.searchboxinput.xiQnY').fill(`${place}`);
+	await page.locator('#searchbox-searchbutton').click();
     await page.waitForFunction(
         () => {
             return document.querySelector('.L1xEbb') || document.querySelector('.yx21af.XDi3Bc');
         },
-        { timeout: 30000 }
+        { timeout: 50000 }
     );
 
 	const isList = await page.evaluate(() => {
 		const resultEl = document.querySelector('.fontTitleLarge.IFMGgb');
 		const detailEl = document.body.querySelector('.yx21af.XDi3Bc');
-		if ( resultEl && !detailEl ) {
-			return true;
-		} else {
-			return false;
-		}
+		return ( resultEl && !detailEl);
 	});
 
-	console.log(isList);
 	if(isList){
 		await page.waitForSelector('.Nv2PK.THOPZb.CpccDe');
 		const places = await page.evaluate(() => {
-			const listBox = document.body.querySelectorAll(".Nv2PK.THOPZb.CpccDe");
-			const results = Array.from(listBox).map(item => {
-				const anchor = item.querySelector('.hfpxzc').getAttribute('aria-label');
-				return anchor;
+			const listBox = Array.from(document.body.querySelectorAll(".hfpxzc"));
+			const results = listBox.map(item => {
+				const label = item.getAttribute('aria-label');
+				const href = item.getAttribute('href');
+				return { label: label, href: href };
 			});
-
-			return { nodeList: listBox, stringList: results };
+			return { anchor: results };
 		});
-		
-		const resultString = places.stringList.map((item, index) => `${index} ${item}`).join('\n');
+
+		const resultString = places.anchor.map((item, index) => `${index} ${item.label}`).join('\n');
 		console.log('Here is the list result: \n' + resultString);
+
 		const selectedPlace = prompt('Please input number to select places:');
 
-		if(!selectedPlace){
-			throw new Error('The input cannot be empty');
-		}
-
+		if(!selectedPlace) throw new Error('The input cannot be empty');
 		
-
+		const selectedHref = places.anchor[parseInt(selectedPlace)].href;
+		selectedUrl = selectedHref;
 
 	} else {
 		await page.waitForFunction(() => window.location.href.includes('?entry=ttu'), { timeout: 30000 });
 		const currentURl = await page.evaluate(() => {
 			return window.location.href;
 		});
-		console.log(currentURl);
+		selectedUrl = currentURl;
 	}
-	
 
 	await browser.close();
 
+	return selectedUrl;
 };
 
 const getReview = async (url) => {
-	const browser = await pw.chromium.launch({ headless: true });
+	const browser = await pw.chromium.launch({ headless: false });
 	const page = await browser.newPage();
 	let reviews = [];
 
@@ -101,8 +90,7 @@ const getReview = async (url) => {
 
 	await browser.close();
 
-	console.log(reviews);
-	return reviews;
+	return reviews.length > 0 ? reviews.join('\n') : 'Sorry currently there is no review available';
 };
 
 const run = async (keyword) => {
@@ -111,30 +99,41 @@ const run = async (keyword) => {
 	// Get url from selected place
 	const url = await getPlace(keyword);
 
+	console.log('final url is:',url);
+
 	//get reviews list
-	// const reviews = await getReview(url);
+	const reviews = await getReview(url);
 
 	console.log('===== One Process Finished =====');
 
-	// const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+	console.log('review is:', reviews);
 
-	let firstPrompt = `I have collected some reviews of a place I was considering visiting.\n
-          Can you summarize the reviews for me?. The reviews are bellow:`;
+	if( reviews.split(' ')[0] == 'Sorry' ) {
+		console.log(reviews);
+		throw new Error ('No review available');
+	} else {
+		
+		const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
-	let indoPrompt = `Saya sudah mengumpulkan beberapa review dari tempat yang akan saya kunjungi. \n
-          Tolong, bisakah kamu membuat kesimpulan dari review tersebut untuk saya?. Adapun reviewnya seperti ini:`;
+		let firstPrompt = `I have collected some reviews of a place I was considering visiting.\n
+		Can you summarize the reviews for me?. The reviews are bellow:`;
 
-	let dataPrompt = '';
+		let indoPrompt = `Saya sudah mengumpulkan beberapa review dari tempat yang akan saya kunjungi. \n
+			Tolong, bisakah kamu membuat kesimpulan dari review tersebut untuk saya?. Adapun reviewnya seperti ini:`;
 
-	// for (let i = 0; i <= reviews.length; i++) {
-	// 	dataPrompt += reviews[i];
-	// }
+		let dataPrompt = '';
 
-	// const result = await model.generateContent(firstPrompt + dataPrompt);
-	// const response = await result.response;
-	// const text = response.text();
-	console.log('===== Done. =====');
-	// console.log(text);
+		for (let i = 0; i <= reviews.length; i++) {
+			dataPrompt += reviews[i];
+		}
+
+		const result = await model.generateContent(firstPrompt + dataPrompt);
+		const response = await result.response;
+		const text = response.text();
+		console.log(text);
+		console.log('===== Done. =====');
+		return;
+	}
 };
 
 const keyword = prompt('Search for place here:');
